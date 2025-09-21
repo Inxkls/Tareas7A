e7c274248c2c47fe8ff93acafd4ba9c0004a203e
Inxkls <202260599@ucc.mx>
2025-09-20 20:59:20 -0600

    CalculadoraIMC

--- App.js
+++ App.js
@@ -1,62 +1,32 @@
-import { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HistoryScreen from "./screen/HistoryScreen";
+import HomeScreen from "./screen/HomeScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [users, setUsers]= useState([])
-  const[loading, setLoading]=useState(true)
-
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response=>response.json())
-    .then(data=> {
-      setUsers(data)
-      setLoading(false)
-    })
-  },[])
-
-if(loading){
-  return(<View style={styles.center}> <Text >Cargando...</Text> 
- <ActivityIndicator size ='large' color= 'rgba(80, 124, 91, 1)'/>
-  </View> )
-}
-
   return (
-    <View style={styles.container}>
-      <Text>Consume API</Text>
-      <FlatList  
-        data={users}
-        renderItem={({item})=><View style={styles.item}>
-          <Text>{item.title}</Text>
-          <Text>{item.completed ? "✅" : "❌"}</Text>
-        </View>}
-        keyExtractor={item=>String(item.id)}
-      />
-
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Calculadora") {
+              iconName = "calculator";
+            } else if (route.name === "Historial") {
+              iconName = "time";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#192736ff",
+          tabBarInactiveTintColor: "blue",
+        })}
+      >
+        <Tab.Screen name="Calculadora" component={HomeScreen} />
+        <Tab.Screen name="Historial" component={HistoryScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ddb4dbff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  center:{
-    flex:1,
-    backgroundColor: '#594264ff',
-    alignItems:'center',
-    justifyContent:'center',
-
-  }, 
-  item:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    backgroundColor:"#a2d2ff", 
-    padding:20,
-    marginVertical:8, 
-    marginHorizontal:16,  
-    borderRadius:10,
-  }
-});
